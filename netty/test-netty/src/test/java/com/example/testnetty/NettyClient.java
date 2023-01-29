3aa0febe99a82dd490202639846504dccdffadec
binge <826819173@qq.com>
2023-01-29 11:19:10 +0800

    玩玩netty 有空再说

---
+++ netty/test-netty/src/test/java/com/example/testnetty/NettyClient.java
@@ -0,0 +1,13 @@
+package com.example.testnetty;
+
+/**
+ * @program: nb-demo
+ * @description:
+ * @author: HJB
+ * @create: 2023-01-29 11:18
+ **/
+public class NettyClient {
+    public static void main(String[] args) {
+
+    }
+}
